254588bb31c9c8bf1fcac1f0f7034ad3e3ee0f40
Luis Valladolid <105667043+LuisVad@users.noreply.github.com>
2023-03-15 14:38:55 -0600

    Delete ReactNative directory

--- ReactNative/App Airbnb/Airbnb/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Navigation from "./config/navigation/Navigation";
-import {app} from './config/firebase'
-
-export default function App() {
-  return (
-    <Navigation/>
-  );
-}
